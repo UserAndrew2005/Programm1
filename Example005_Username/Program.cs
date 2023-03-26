558ee6056447cb2f6269d0f24a4feecbcf4520f9
dron andrew <dronandrij637@gmail.com>
2023-03-27 00:14:50 +0300

    Example005

---
+++ Example005_Username/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите имя");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+Console.WriteLine("Привет Машенька");
+}
+else
+{
+Console.WriteLine("Пшел вон отсюда!):");
+}
